a600b4f7bbd2f98953d644b7bea200c400ad3b95
themohit96 <mohit.saxena1596@gmail.com>
2019-03-07 10:58:19 +0530

    testing  commit

--- app/src/main/java/com/thegiant/sevenmarch/MainActivity.java
+++ app/src/main/java/com/thegiant/sevenmarch/MainActivity.java
@@ -3,18 +3,38 @@ package com.thegiant.sevenmarch;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText editText;
+    private EditText mail,pass;
+    private Button login;
+    String dMail,dPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        editText=findViewById(R.id.editText);
+        mail=findViewById(R.id.editText);
+        pass=findViewById(R.id.editText2);
+        login=findViewById(R.id.button);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mail.getText().toString().equals(dMail)&&pass.getText().toString().equals(dPass)){
+                    Toast.makeText(MainActivity.this, "Correct, Login successful", Toast.LENGTH_SHORT).show();
+
+                }else if (mail.getText().toString().equals("")||pass.getText().toString().equals("")){
+                    Toast.makeText(MainActivity.this, "Please enter id and password", Toast.LENGTH_SHORT).show();
+                }else {
+                    Toast.makeText(MainActivity.this, "Incorrect id or password!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     @Override
@@ -23,8 +43,8 @@ public class MainActivity extends AppCompatActivity {
 
         SharedPreferences sharedPreferences=getSharedPreferences("themohit96.sharedprefdemo",MODE_PRIVATE);
         SharedPreferences.Editor editor=sharedPreferences.edit();
-        editor.putString("name",editText.getText().toString().trim());
-        editor.putBoolean("bool",true);
+        editor.putString("mail","mohit.saxena_mca18@gla.ac.in");
+        editor.putString("pass","123456");
         editor.apply();
     }
 
@@ -32,9 +52,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onResume() {
         super.onResume();
         SharedPreferences sharedPreferences=getSharedPreferences("themohit96.sharedprefdemo",MODE_PRIVATE);
-        String string=sharedPreferences.getString("name","user");
-        Boolean remem=sharedPreferences.getBoolean("bool",false);
-        Toast.makeText(getApplicationContext(), "bool"+remem, Toast.LENGTH_SHORT).show();
-        editText.setText(string);
+        dMail=sharedPreferences.getString("mail","user");
+        dPass=sharedPreferences.getString("pass","password");
+
     }
+
+
 }
